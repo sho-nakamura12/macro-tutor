58500bc117ca967d63f5e7028b4f18e54da53240
翔 <you@example.com>
2026-05-24 13:45:35 +0900

    admin: 先生ごとのデータ表示制限を追加

--- myproject/macrotutor/admin.py
+++ myproject/macrotutor/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Student, Unit, UnitMaster
 
+# Studentの表示ルールをカスタマイズします
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'grade', 'teacher') 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(teacher=request.user)
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ('student', 'unit_master', 'status')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(student__teacher=request.user)
+
 admin.site.register(UnitMaster)
-admin.site.register(Student)
-admin.site.register(Unit)
